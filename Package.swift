a335009ad0f1f836fc678d6b3477f53f4afca593
jungHm <jungheum.ahn@wesang.com>
2026-03-05 18:06:20 +0900

    CTrueTime도 접근 가능하게 타깃 추가

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "TrueTime",
-            targets: ["TrueTime"]
+            targets: ["TrueTime", "CTrueTime"]
         )
     ],
     targets: [
